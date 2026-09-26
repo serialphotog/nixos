b2ba9f581f04d73c8d28641e7d93a6003923312a
Adam Thompson <adam@hackeradam.com>
2026-09-26 04:23:52 -0600

    Added some performance improvements for Lappy

--- hosts/Lappy/configuration.nix
+++ hosts/Lappy/configuration.nix
@@ -14,6 +14,10 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  hardware.graphics.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.thermald.enable = true;
+
   networking.hostName = "Lappy";
 
   system.stateVersion = "26.05";
